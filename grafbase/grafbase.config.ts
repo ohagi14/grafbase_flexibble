0945ad419a3ebbd0ffc7633a85dfb88d1a4d6a3b
ohagi14 <furikake.hs@gmail.com>
2023-06-28 17:35:37 +0900

    ⚡️

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -9,9 +9,10 @@ const User = g.model('User', {
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(), 
   projects: g.relation(() => Project).list().optional(),
-}).auth((rules) => {
-  rules.public().read()
 })
+// .auth((rules) => {
+//   rules.public().read()
+// })
 
 // @ts-ignore
 const Project = g.model('Project', {
@@ -22,20 +23,21 @@ const Project = g.model('Project', {
   githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
 })
+// .auth((rules) => {
+//   rules.public().read()
+//   rules.private().create().delete().update()
+// })
 
-const jwt = auth.JWT({
-  issuer: 'grafbase',
-  secret:  g.env('NEXTAUTH_SECRET')
-})
+// const jwt = auth.JWT({
+//   issuer: 'grafbase',
+//   secret:  g.env('NEXTAUTH_SECRET')
+// })
 
-export default config({
-  schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private()
-  },
-})
+// export default config({
+//   schema: g,
+//   auth: {
+//     providers: [jwt],
+//     rules: (rules) => rules.private()
+//   },
+// })
